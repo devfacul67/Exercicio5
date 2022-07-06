ae627ecfddab15b4a331eaca8fca71c46836062f
curso.ads <curso.ads@LAB06-07>
2022-07-06 18:27:25 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+            Console.WriteLine("Programa para encontrar todos os numeros pares de 1 a 100." +
+                "\n Aperte qualquer tecla para continuar...");
+            Console.ReadKey();
+
+            while (cont <= 100)
+            {
+                if (cont % 2 == 0)
+                {
+                    Console.WriteLine($"Número {cont}");
+                    cont++;
+                }
+                else
+                {
+                    cont++;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
